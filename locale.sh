11e61222cdb775a8f9804eec2c61c54894ba8aff
user4022 <personal@j-baum.at>
2021-12-10 17:19:57 +0100

    locale-test

--- locale.sh
+++ locale.sh
@@ -4,8 +4,13 @@ arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 arch-chroot /mnt hwclock --systohc
 
 
+echo de_DE.UTF-8 >> /mnt/etc/locale.gen
+
 arch-chroot /mnt localectl set-locale LANG=de_DE.UTF-8
 
+locale-gen
+
+exit
 touch /mnt/etc/X11/xorg.conf.d/20-keyboard.conf
 cat <<EOF> /mnt/etc/X11/xorg.conf.d/20-keyboard.conf
 
