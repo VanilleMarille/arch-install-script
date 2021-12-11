42c1200daf4e3c55988c35953c628868a3efcaa6
user4022 <personal@j-baum.at>
2021-12-11 16:30:12 +0100

    Fixed Autologin

--- login/lightdm.sh
+++ login/lightdm.sh
@@ -6,8 +6,8 @@ arch-chroot /mnt pacman -S --noconfirm lightdm lightdm-gtk-greeter
 arch-chroot /mnt systemctl enable lightdm.service 
 
 
-groupadd autologin
-usermod -a -G autologin $USERNAME
+arch-chroot /mnt groupadd autologin
+arch-chroot /mnt usermod -a -G autologin $USERNAME
 
 sed -i "s/#autologin-user.*/autologin-user=$USERNAME/" /mnt/etc/lightdm/lightdm.conf 
 
