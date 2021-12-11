4e8eda5aed247d94f6a87e82302f9762354f3289
user4022 <personal@j-baum.at>
2021-12-11 16:33:09 +0100

    Removed unused lines

--- environment/openbox.sh
+++ environment/openbox.sh
@@ -5,16 +5,4 @@ USERNAME=username
 arch-chroot /mnt pacman -S openbox ttf-dejavu ttf-liberation
 arch-chroot /mnt pacman -S mate-terminal
 
-touch /mnt/home/$USERNAME/.xinitrc
-cat <<EOF> /mnt/home/$USERNAME/.xinitrc
-export XDG_CURRENT_DESKTOP=Budgie:GNOME
-exec budgie-desktop
-EOF
-
-touch /mnt/home/$USERNAME/.zshrc
-cat <<EOF> /mnt/home/$USERNAME/.zshrc
-if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-  exec startx
-fi
-EOF
 
