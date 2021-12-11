d2728d1d6cebe633ba6615d78708a63574d817e4
user4022 <personal@j-baum.at>
2021-12-11 17:59:24 +0100

    Optimized Lightdm

--- environment/openbox.sh
+++ environment/openbox.sh
@@ -2,7 +2,22 @@
 
 USERNAME=username
 
-arch-chroot /mnt pacman -S openbox ttf-dejavu ttf-liberation
-arch-chroot /mnt pacman -S mate-terminal
+arch-chroot /mnt pacman -S openbox ttf-dejavu ttf-liberation xterm xorg-xrandr
+
+touch /mnt/opt/xrandr.sh
+cat <<EOF> /mnt/opt/xrandr.sh
+#!/bin/bash
+
+xrandr --size 1920x1080
+
+EOF
+
+chmod +x /mnt/opt/xrandr.sh
+
+
+sed '/^[Seat:*]/a display-setup-script=/opt/xrandr.sh/g' /etc/lightdm/lightdm.conf
+
+sed '/^[Seat:*]/a session-setup-script=/opt/xrandr.sh/g' /etc/lightdm/lightdm.conf
+
 
 
