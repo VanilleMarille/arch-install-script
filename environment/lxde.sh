17ff05f9a5f89f27b68d5a05a8f91d71f40eafb3
user4022 <personal@j-baum.at>
2021-12-18 17:14:33 +0100

    added lxde

---
+++ environment/lxde.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERNAME=username
+
+arch-chroot /mnt pacman -S --noconfirm lxde-common lxsession openbox alacritty 
+
+#touch /mnt/home/$USERNAME/.xinitrc
+#cat <<EOF> /mnt/home/$USERNAME/.xinitrc
+#export XDG_CURRENT_DESKTOP=Budgie:GNOME
+#exec budgie-desktop
+#EOF
+
+#touch /mnt/home/$USERNAME/.zshrc
+#cat <<EOF> /mnt/home/$USERNAME/.zshrc
+#if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+#  exec startx
+#fi
+#EOF
+
