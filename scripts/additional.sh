b2d60e334838350742191a02634622acfaf07734
user4022 <personal@j-baum.at>
2021-12-24 12:40:59 +0100

    Added links browser

--- scripts/additional.sh
+++ scripts/additional.sh
@@ -4,3 +4,4 @@ arch-chroot /mnt pacman -Syu --noconfirm ttf-dejavu
 arch-chroot /mnt pacman -Syu --noconfirm python
 arch-chroot /mnt pacman -Syu --noconfirm xdotool
 arch-chroot /mnt pacman -Syu --noconfirm git
+arch-chroot /mnt pacman -Syu --noconfirm links
