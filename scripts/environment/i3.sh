221d7bdf031702772fbae59deed8acd4a7f97a7c
user4022 <personal@j-baum.at>
2021-12-31 22:01:41 +0100

    added wlan support

--- scripts/environment/i3.sh
+++ scripts/environment/i3.sh
@@ -2,3 +2,6 @@
 
 arch-chroot /mnt pacman -S --noconfirm i3-gaps i3status i3lock dmenu
 arch-chroot /mnt pacman -S --noconfirm kitty feh
+
+#Uncomment for wlan
+#arch-chroot /mnt pacman -S --noconfirm iwd
