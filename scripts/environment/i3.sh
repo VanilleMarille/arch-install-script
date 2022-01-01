7151586b6b07b3515c8be8ffe15b75634e1b09db
user4022 <personal@j-baum.at>
2022-01-01 20:11:39 +0100

    Added ntfs to i3

--- scripts/environment/i3.sh
+++ scripts/environment/i3.sh
@@ -2,6 +2,7 @@
 
 arch-chroot /mnt pacman -S --noconfirm i3-gaps i3status i3lock dmenu
 arch-chroot /mnt pacman -S --noconfirm pulseaudio pulsemixer ffmpeg
+arch-chroot /mnt pacman -S --noconfirm ntfs-3g
 arch-chroot /mnt pacman -S --noconfirm kitty feh
 
 #Uncomment for wlan
