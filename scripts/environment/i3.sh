2998f4182e6eb22ad77c6500507bf16b690bb972
user4022 <personal@j-baum.at>
2022-01-01 20:05:50 +0100

    Added ffmpeg

--- scripts/environment/i3.sh
+++ scripts/environment/i3.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 arch-chroot /mnt pacman -S --noconfirm i3-gaps i3status i3lock dmenu
+arch-chroot /mnt pacman -S --noconfirm pulseaudio pulsemixer ffmpeg
 arch-chroot /mnt pacman -S --noconfirm kitty feh
 
 #Uncomment for wlan
 #arch-chroot /mnt pacman -S --noconfirm iwd
-#Uncomment for sound
-#arch-chroot /mnt pacman -S --noconfirm pulseaudio pulsemixer
